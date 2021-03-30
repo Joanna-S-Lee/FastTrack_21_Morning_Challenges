443165090e6b838c06ad5ae13e6188987bcf84f6
Joanna-S-Lee <annaoj91@gmail.com>
2021-03-31 08:42:17 +1100

    made further attempts on scrabble and wanderer challenge

--- ruby/scrabble/scrabble.rb
+++ ruby/scrabble/scrabble.rb
@@ -5,34 +5,49 @@ def word_ranking(words)
   #return the first
 end
 
-letters = {
-  "a" => "1"
-  "b" => "2"
-  "c" => "3"
-  "d" => "4"
-  "e" => "5"
-  "f" => "6"
-  "g" => "7"
-  "h" => "8"
-  "i" => "9"
-  "j" => "10"
-  "k" => "11"
-  "l" => "12"
-  "m" => "13"
-  "n" => "14"
-  "o" => "15"
-  "p" => "16"
-  "q" => "17"
-  "r" => "18"
-  "s" => "19"
-  "t" => "20"
-  "u" => "21"
-  "v" => "22"
-  "w" => "23"
-  "x" => "24"
-  "y" => "25"
-  "z" => "26"
-}
+letters = []
+('a'..'z').each_with_index.to_a do |letters, index|
+
+
+
+
+if highest_word_score < current_word_score
+  highest_word_score = current_word_score
+  
+end 
+
+word = 'reshow the shower'
+word.split('')
+word_ranking('reshow the shower')
+
+# letters = {
+#   "a" => "1"
+#   "b" => "2"
+#   "c" => "3"
+#   "d" => "4"
+#   "e" => "5"
+#   "f" => "6"
+#   "g" => "7"
+#   "h" => "8"
+#   "i" => "9"
+#   "j" => "10"
+#   "k" => "11"
+#   "l" => "12"
+#   "m" => "13"
+#   "n" => "14"
+#   "o" => "15"
+#   "p" => "16"
+#   "q" => "17"
+#   "r" => "18"
+#   "s" => "19"
+#   "t" => "20"
+#   "u" => "21"
+#   "v" => "22"
+#   "w" => "23"
+#   "x" => "24"
+#   "y" => "25"
+#   "z" => "26"
+# }
 
 # individual word score: 1) Get individual characters from the user input. 2) add the value of characters together
 
--- ruby/the_wanderer/wanderer.rb
+++ ruby/the_wanderer/wanderer.rb
@@ -1,3 +1,17 @@
 def direction_saver(travelers_path)
 	# A journey of a thousand miles begins with a single step...
+	directions = [ 'NORTH', 'SOUTH', 'EAST', 'WEST']
+		
+	input = ['NORTH', 'SOUTH']
+	output = []	
+		
+	input = ['EAST', 'WEST']
+	output = []			
+	
+
+	directions2 = ['NORTH', 'SOUTH', 'SOUTH', 'WEST']
+	directions2.delete('NORTH','SOUTH')
+	directions2
+	
+		
 end
